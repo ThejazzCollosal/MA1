c14d520e286240f9a7db441b7334ae9743e7cabd
ThejazzCollosal <114967609+ThejazzCollosal@users.noreply.github.com>
2025-01-27 13:43:04 +0800

    Update finalscene.js

--- Jazfe22/finalscene.js
+++ Jazfe22/finalscene.js
@@ -17,13 +17,24 @@ class finalscene extends Phaser.Scene {
   
       const image = this.add.image(320,208,"finalscene").setScale(1);
     
-      var spaceDown = this.input.keyboard.addKey("SPACE");
+      //var spaceDown = this.input.keyboard.addKey("SPACE");
+      var KeyY = this.input.keyboard.addKey(89);
+      var KeyN = this.input.keyboard.addKey(78);
+
   
     //   On spacebar event, call the world scene
   
-      spaceDown.on('down', function () {
+    //   spaceDown.on('down', function () {
+    //     this.scene.start("yesscene");
+    // }, this)
+
+         KeyN.on(
+      "down", function () {
         this.scene.start("yesscene");
-    }, this)
+
+          KeyY.on(
+      "down", function () {
+        this.scene.start("death");
 
 //     key1.on('down', function () {
 //         this.scene.start("death");
