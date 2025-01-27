cc7614ce4080743ede33d674ec9ffbb0131c8d08
ThejazzCollosal <114967609+ThejazzCollosal@users.noreply.github.com>
2025-01-27 17:16:51 +0800

    Update level4.js

--- Jazfe22/level4.js
+++ Jazfe22/level4.js
@@ -172,6 +172,7 @@ class level4 extends Phaser.Scene {
         if (this.player.x > 10 && this.player.x < 32 && this.player.y < 235.4 && this.player.y > 210) {
             if (window.note >= 3) {
                 this.level5()
+             }
         }
 
         if (this.player.x > 486 && this.player.x < 538 && this.player.y < 394 && this.player.y > 389) {
